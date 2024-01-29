dc21a0d9cb39b874ef5947455aa8ab89c2e4536e
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-29 12:02:25 -0600

    Definiendo rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Models\Notification;
+use App\Models\Priority;
+use App\Models\Profession;
+use App\Models\Summary;
+use App\Models\Task;
+use App\Models\User;
+use App\Models\Workspace;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +23,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function(){
+    Route::resource('users', User::class);
+    Route::resource('workspaces', Workspace::class);
+    Route::resource('tasks', Task::class);
+    Route::resource('notifications', Notification::class);
+    Route::resource('professions', Profession::class);
+    Route::resource('priorities', Priority::class);
+    Route::resource('summaries', Summary::class);
 });
