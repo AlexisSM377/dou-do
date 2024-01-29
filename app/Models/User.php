5ff7785660a070d2ab7dcb5adbad10e1bdc251fe
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-29 10:58:55 -0600

    Flujo de relaciones para professions

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Notification::class);
     }
+
+    public function profesion()
+    {
+        return $this->belongsTo(Profession::class);
+    }
 }
