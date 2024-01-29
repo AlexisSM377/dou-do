44052d6704ffa117debc2f1920e55fb3240035ef
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-29 11:04:51 -0600

    Flujo de relaciones para tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,8 +9,22 @@ class Task extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'priority_id',
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+
     public function priority()
     {
         return $this->belongsTo(Priority::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Summary::class);
     }
+
+    public function tasks()
+    {
+        return $this->belongsToMany(Task::class);
+    }
 }
