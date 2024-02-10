35249b0e1d03946d7cc3ed48e9ba6a64f66e1cb6
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-10 12:52:16 -0600

    Ajustes

--- app/Http/Resources/Resources/UserResource.php
+++ app/Http/Resources/Resources/UserResource.php
@@ -19,7 +19,6 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'name' => $this->name,
             'last_name' => $this->last_name,
             'email' => $this->email,
