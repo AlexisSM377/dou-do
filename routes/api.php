4bc57a5e9d89abf35baf5b07cdd5af9a4dc41fde
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-12 19:51:18 -0600

    Ajuste

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\SummaryController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkspaceController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
