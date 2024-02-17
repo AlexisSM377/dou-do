8da0d5b3084ded4b3fbe5de0a0599d24c89a1a03
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-17 17:53:39 -0600

    Ajuste

--- app/Http/Controllers/VerifyEmailController.php
+++ app/Http/Controllers/VerifyEmailController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class VerifyEmailController extends Controller
 {
-    public function getVerifyRequest(Request $request, $token)
+    public function getVerifyRequest(Request $request, $body)
     {
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+        if ($request->hasValidSignature()) {
+            $rescuedBody = json_decode(Crypt::decryptString($body));
+            if ($rescuedBody->token && $rescuedBody->request_code) {
+                
+            }
+        } else {
 
+        }
     }
 }
