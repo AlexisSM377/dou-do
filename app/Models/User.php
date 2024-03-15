351bb24075468d2811f77b5720c7f6572399db11
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-03-15 09:56:43 -0600

    Preparando logica para almacenar ExpoPushToken

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'last_name',
+        'expo_push_token',
         'email',
         'password',
         'birthdate',
