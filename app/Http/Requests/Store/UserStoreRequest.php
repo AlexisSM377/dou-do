671bf9ce8710d8d36856d990054bb6c6c4d311fd
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-04-04 10:49:21 -0600

    Ajuste previo

--- app/Http/Requests/Store/UserStoreRequest.php
+++ app/Http/Requests/Store/UserStoreRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 /**
  * Store Request to validate the User fields
  */
-class UserStoreRequest extends FormRequest
+class   UserStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
